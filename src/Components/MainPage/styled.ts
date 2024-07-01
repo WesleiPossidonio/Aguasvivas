06e53bf24ed1e1f68f5e54ae91d87ec02e22b8b1
Weslei França <weslwifranca@gmail.com>
2024-07-01 13:14:28 -0300

    update mainpage add nem banner

--- src/Components/MainPage/styled.ts
+++ src/Components/MainPage/styled.ts
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
+import banner from '../../assets/banner1.jpg'
+
 export const ContainerMainPage = styled.section`
 width: 100%;
 height: 100vh;
 
-background-image: url('http://www.farmaciaaguaviva.com/site/images/banner1.jpg');
+background-image: url(${banner});
 background-position: center center;
 background-repeat: no-repeat;
 background-size: cover;
